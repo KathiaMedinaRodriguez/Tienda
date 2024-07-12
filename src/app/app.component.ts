14287e7de5a7d18f9b8324e5aed123a9f6edbab7
Kathia Medina Rodriguez <kmedinar1@upao.edu.pe>
2024-07-12 15:01:49 -0500

    feature/calculadora-imc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Product} from "./product";
-import {NgForOf} from "@angular/common";
-import {ProductListComponent} from "./product-list/product-list.component";
+import {DecimalPipe, NgForOf, NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, ProductListComponent],
+  imports: [RouterOutlet, NgForOf, NgIf, DecimalPipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Prueba-A';
-}
+  Peso: number = 0;
+  Altura: number = 0;
+  imc: number | null = null;
+  message: string = '';
+
+  calculateIMC() {
+    if (this.Peso > 0 && this.Altura > 0) {
+      this.imc = this.Peso / ((this.Altura / 100) ** 2);
+      this.setMessage();
+    } else {
+      this.message = 'Por favor ingrese peso y altura válidos';
+      this.imc= null;
+    }
+  }
+
+    setMessage()
+    {
+      if (this.imc !== null) {
+        if (this.imc < 18.5) {
+          this.message = 'Bajo de peso';
+        } else if (this.imc >= 18.5 && this.imc < 24.9) {
+          this.message = 'Peso Normal';
+        } else if (this.imc >= 25 && this.imc < 29.9) {
+          this.message = 'Exceso de peso';
+        } else {
+          this.message = 'Obesidad';
+        }
+      }
+    }
+  }
+
